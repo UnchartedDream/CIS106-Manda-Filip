88074d9a3e52f759e1c8b870a1fb52ecb73a6c46
UnchartedDream <143147895+UnchartedDream@users.noreply.github.com>
2023-11-24 19:39:24 -0600

    Add files via upload

---
+++ PS11P4.py
@@ -0,0 +1,50 @@
+def displaynames (name, ave):
+  for i in name, ave:
+    print(i)
+
+#search function
+def seqsearch(name, ave):
+  l = len(name)
+  sindex = -1
+  for y in range(0,l,1):
+    if name[y] == sname:
+      sindex = y
+  return sindex
+  
+#run file
+f = open("BattingAve.txt", "r")
+
+#define arrays
+name = []
+ave = []
+
+lastname = f.readline()
+
+#While loop for knowing what line is what
+while lastname != "":
+  name.append(str(lastname).rstrip("\n"))
+  s = float(f.readline())
+  ave.append(s)
+  lastname = f.readline()
+
+#close file
+f.close()
+
+displaynames (name, ave)
+
+#run program
+ask = input("Do you want to run this program? y/n: ")
+
+while ask =="Yes" or ask == "y":
+  sname = input("What name would you like to look up?: ")
+  i = seqsearch(name, sname)
+  if i == -1:
+    print(sname, "not in the array") #print(sname, "Name not found")
+  else:
+    print (name[i], "batting average of ", ave[i])
+  ask = input("Do you want to run this program? y/n: ")
+
+    
+
+
+
